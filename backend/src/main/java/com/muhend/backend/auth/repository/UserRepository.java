405b594fbc17f19ab1dc47dbc822f7d09234a96d
Muhend <hamohand@gmail.com>
2025-10-27 10:12:55 +0100

    Correction UserRepository

--- backend/src/main/java/com/muhend/backend/auth/repository/UserRepository.java
+++ backend/src/main/java/com/muhend/backend/auth/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.muhend.backend.auth.repository;
 
 import com.muhend.backend.auth.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
     // Search for a user by email
     User findByEmail(String email);
 
