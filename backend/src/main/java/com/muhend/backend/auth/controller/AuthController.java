7457a2de7178195c345d065f022130521da5df49
Muhend <hamohand@gmail.com>
2025-11-05 18:59:23 +0100

    Problème Cors, Les gérer uniquement SecurityConfig

--- backend/src/main/java/com/muhend/backend/auth/controller/AuthController.java
+++ backend/src/main/java/com/muhend/backend/auth/controller/AuthController.java
@@ -16,7 +16,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @Slf4j
-@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
