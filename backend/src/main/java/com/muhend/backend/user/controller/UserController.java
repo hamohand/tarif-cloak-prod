75cc997a22b47c5d97effbeff31b3856d3fede46
Muhend <hamohand@gmail.com>
2025-11-10 13:02:25 +0100

    Plans tarifaires: Starter, Professional, Enterprise

--- backend/src/main/java/com/muhend/backend/user/controller/UserController.java
+++ backend/src/main/java/com/muhend/backend/user/controller/UserController.java
@@ -17,6 +17,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
